2d8439ea2cfe6b9f16e8cfe66528ebca9fb9b883
levonasatryan <levonasatryan28@gmail.com>
2024-10-22 23:54:01 +0400

    adds db layer in main package (yet)

--- server/main.go
+++ server/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Example struct {
@@ -15,7 +22,50 @@ type Example struct {
 }
 
 func main() {
-	fmt.Println("hello motherfucker")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	log.Println("got mongodb url as: ", uri)
+
+	// DB part
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable in .env file.")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("feature_flags").Collection("ffs")
+	title := "Back to the Future"
+	res, err := coll.InsertOne(context.TODO(), bson.D{
+		{"name", "feature 1"},
+		{"value", true},
+	})
+
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", title)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(res, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+
+	// Server part
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{

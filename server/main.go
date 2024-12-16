e92560f2aa47c19ffaa98df030e32b1769010872
Henry <henrygrigoryan5@gmail.com>
2024-12-16 14:09:00 +0400

    add logger & recovery middleware, polish main file

--- server/main.go
+++ server/main.go
@@ -3,22 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+		log.Fatal("Error loading .env file")
 	}
+}
+
+func main() {
 
 	// Server part
 	r := gin.Default()
+	// Use Logger middleware and Recovery middleware
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run("localhost:3030")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err := r.Run(":" + port)
+
+	if err != nil {
+		panic(err)
+	}
 }
